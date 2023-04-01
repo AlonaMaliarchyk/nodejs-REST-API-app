808c6a4841ba83026badbd50c6fa309e007e267d
AlonaMaliarchyk <alenasymorot@gmail.com>
2023-04-01 09:22:25 +0400

    hw #2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,79 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
+const contacts = require("../../models/contacts");
+const router = express.Router();
+const {HttpError} = require("../../helpers");
 
-const router = express.Router()
+const addSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+});
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    try {
+        const allContacts = await contacts.listContacts();
+        res.json(allContacts);
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    try {
+        const {contactId} = req.params;
+        const oneContact = await contacts.getContactById(contactId);
+        if (!oneContact) {
+            throw HttpError(404, `Book with ${contactId} not found`);
+        }
+        res.status(200).json(oneContact);
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    try {
+        const {name, email, phone} = req.body;
+        const {error} = addSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const newContact = await contacts.addContact({name, email, phone});
+        res.status(201).json(newContact)
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    try {
+        const {contactId} = req.params;
+        const deleteById = await contacts.removeContact(contactId);
+        if (!deleteById) {
+            throw HttpError(404, `Book with ${contactId} not found`);
+        }
+        res.json({message: "Contact deleted"})
+    } catch (error) {
+        next(error);
+    }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+    try {
+        const {contactId} = req.params;
+        const {error} = addSchema.validate(req.body);
+        if (error) {
+            throw HttpError(400, error.message);
+        }
+        const updateContact = await contacts.updateContact(contactId, req.body);
+        if (!updateContact) {
+            throw HttpError(404, `Book with ${contactId} not found`);
+        }
+        res.status(200).json(updateContact)
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router
